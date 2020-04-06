fe3291d2bef1bd67f7da8b9d49ad258aefe8c6c2
Uizuk <uizuk94@gmail.com>
2020-04-06 21:32:39 +0200

    CHange return msg

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# main.py
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,15 +5,15 @@ app = FastAPI()
 
 @app.get("/")
 def hello_world():
-    return {"message": "Hello World during the coronavirus pandemic}
+    return {"message": "Hello Hello World during the coronavirus pandemic!"}
 
-# class HelloNameResponse(BaseModel):
-#     message: str 
+class HelloNameResponse(BaseModel):
+    message: str 
 
-# @app.get('/hello/{name}', response_model=HelloNameResponse)
-# def hello_name(name:str):
-#     return HelloNameResponse(message=f"Hello {name}")
-#     # return {"message": f"Hello {name}"}
+@app.get('/hello/{name}', response_model=HelloNameResponse)
+def hello_name(name:str):
+    return HelloNameResponse(message=f"Hello {name}")
+    # return {"message": f"Hello {name}"}
 
 #odpalenie appki:
 #uvicorn app:app
