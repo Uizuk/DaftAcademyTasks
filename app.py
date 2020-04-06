e7e0da3cecf67c9c9503cd14ff2328292486dee7
Uizuk <uizuk94@gmail.com>
2020-04-06 21:19:51 +0200

    Add app essentials

---
+++ app.py
@@ -0,0 +1,20 @@
+# main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World"}
+
+class HelloNameResponse(BaseModel):
+    message: str 
+
+@app.get('/hello/{name}', response_model=HelloNameResponse)
+def hello_name(name:str):
+    return HelloNameResponse(message=f"Hello {name}")
+    # return {"message": f"Hello {name}"}
+
+#odpalenie appki:
+#uvicorn app:app
